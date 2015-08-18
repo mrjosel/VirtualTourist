ec64db32c7ebc2e2b87ef1fea774fdbfeac4770a
brianjosel <brian.josel@gmail.com>
2015-08-18 13:24:27 -0400

    fix pinDrop bug add animation to pinDrop need to fix pinDrop radius bug

--- VirtualTourist/TravelLocationsMapViewController.swift
+++ VirtualTourist/TravelLocationsMapViewController.swift
@@ -11,11 +11,14 @@ import MapKit
 
 class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate {
     
+    //Map View
     @IBOutlet weak var mapView: MKMapView!
     
-    override func viewWillAppear(animated: Bool) {
-        self.navigationController?.navigationBar.hidden = true
-    }
+    //gesture for dropping pin
+    var pinDropGesture : UILongPressGestureRecognizer?
+    
+    //annotations array
+    var annotations = [MKPointAnnotation]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +27,48 @@ class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate {
         //make vc mapView delegate
         self.mapView.delegate = self
         
+        //set up pinDrop mechanism, make vc delegate
+        self.pinDropGesture = UILongPressGestureRecognizer(target: self, action: "dropPin:")
+        self.mapView.addGestureRecognizer(self.pinDropGesture!)
+        
+    }
+    
+    //create view for annotations
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        //reuseID and pinView
+        let reuseID = "pin"
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseID) as? MKPinAnnotationView
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
+            pinView!.animatesDrop = true
+        } else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    
+    
+    func dropPin(sender: UIGestureRecognizer) {
+        
+        if sender.state == UIGestureRecognizerState.Ended || sender.state == UIGestureRecognizerState.Changed {
+            return
+        } else {
+            //create coordinate object point object
+            let point = sender.locationInView(self.mapView)
+            let coordinates = self.mapView.convertPoint(point, toCoordinateFromView: self.mapView)
+            
+            //create annotation
+            var dropPin = MKPointAnnotation()
+            dropPin.coordinate = coordinates
+            
+            //apend to array
+            self.annotations.append(dropPin)
+            
+            //add to mapview
+            self.mapView.addAnnotation(dropPin)
+        }
     }
 
     override func didReceiveMemoryWarning() {
