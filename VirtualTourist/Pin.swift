e142a1188329f71069cae8e2740083b32dc509e6
brianjosel <brian.josel@gmail.com>
2015-08-19 13:57:25 -0400

    add NSFetchedResultsController add getAnnotations method add Pin NSManagedObject

--- VirtualTourist/Pin.swift
+++ VirtualTourist/Pin.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import CoreData
+import MapKit
+
+@objc
+
+
+//class to persist annotations
+class Pin: NSManagedObject, MKAnnotation {
+    
+    //managed variables
+    @NSManaged var latitude: NSNumber!
+    @NSManaged var longitude: NSNumber!
+    
+    //called in init
+    override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+    }
+    
+    //initializer
+    init(latitude: Double, longitude: Double, context: NSManagedObjectContext){
+        //entitiy for use in CoreData Model
+        let entity = NSEntityDescription.entityForName("Pin", inManagedObjectContext: context)
+        
+        //super class init
+        super.init(entity: entity!, insertIntoManagedObjectContext: context)
+        
+        //set parameters, convert to NSNumbers to allow for an MSMangedObject
+        self.latitude = NSNumber(double: latitude)
+        self.longitude = NSNumber(double: longitude)
+    }
+    
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: self.latitude as! Double, longitude: self.longitude as! Double)
+    }
+}
--- VirtualTourist/TravelLocationsMapViewController.swift
+++ VirtualTourist/TravelLocationsMapViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import MapKit
+import CoreData
 
-class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate {
+class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate, NSFetchedResultsControllerDelegate {
     
     //Map View
     @IBOutlet weak var mapView: MKMapView!
@@ -20,18 +21,96 @@ class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate {
     //annotations array
     var annotations = [MKPointAnnotation]()
     
+    //shared context
+    lazy var sharedContext: NSManagedObjectContext = {
+        
+        //return context from CoreData
+        return CoreDataStackManager.sharedInstance().managedObjectContext!
+        }()
+    
+    //fetched results controller for persisting pins
+    lazy var fetchedResultsController: NSFetchedResultsController = {
+        
+        //create fetch request with sort descriptor
+        let fetchRequest = NSFetchRequest(entityName: "Pin")
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "latitude", ascending: true)]
+        
+        //create controller from fetch request
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.sharedContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        return fetchedResultsController
+        }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        //make vc mapView delegate
+        //set delegates
         self.mapView.delegate = self
+        self.fetchedResultsController.delegate = self
         
         //set up pinDrop mechanism, make vc delegate
         self.pinDropGesture = UILongPressGestureRecognizer(target: self, action: "dropPin:")
         self.mapView.addGestureRecognizer(self.pinDropGesture!)
         
+        //perform fetch
+        self.fetchedResultsController.performFetch(nil)
+        
+        //get any persisted annotations if they exist
+        self.getAnnotations()
+        
     }
+    
+    //add pin to array and to map
+    func dropPin(sender: UIGestureRecognizer) {
+
+        //create coordinate object point object
+        let point = sender.locationInView(self.mapView)
+        let coordinates = self.mapView.convertPoint(point, toCoordinateFromView: self.mapView)
+        
+        //only allow pins to be dropped once, not checking state place pins indefinitely
+        if sender.state == UIGestureRecognizerState.Began {
+            //create annotation
+            var dropPin = MKPointAnnotation()
+            dropPin.coordinate = coordinates
+            
+            //apend to array
+            self.annotations.append(dropPin)
+            
+            //add to mapview
+            self.mapView.addAnnotation(dropPin)
+        }
+    }
+    
+    //get persisted annotations
+    func getAnnotations() {
+
+        //get array of objects from fetchResultsController
+        let sectionInfo = self.fetchedResultsController.sections![0] as! NSFetchedResultsSectionInfo
+        
+        //TODO: WRAP FOR LOOP IN IF != 0 STATEMENT 
+        if sectionInfo.numberOfObjects == 0 {
+            println("no objects persisted")
+            return
+        }
+        
+        //for each persisted pin object
+        for object in sectionInfo as! NSArray {
+            //get persisted latitude and longitude information
+            let lat = (object as! Pin).latitude as! Double
+            let lon = (object as! Pin).longitude as! Double
+            
+            //create coordinate from lat and lon
+            let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+            
+            //create annotation, add coordinate, add to map
+            var pin = MKPointAnnotation()
+            pin.coordinate = coordinate
+            self.mapView.addAnnotation(pin)
+        }
+    }
+    
+    
     
     //create view for annotations
     func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
@@ -55,27 +134,23 @@ class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate {
     }
     
     
-    //add pin to array and to map
-    func dropPin(sender: UIGestureRecognizer) {
-        //keeps from user dragging and adding multiple pins
-        if sender.state == UIGestureRecognizerState.Ended || sender.state == UIGestureRecognizerState.Changed {
-            return
-        } else {
-            //create coordinate object point object
-            let point = sender.locationInView(self.mapView)
-            let coordinates = self.mapView.convertPoint(point, toCoordinateFromView: self.mapView)
-            
-            //create annotation
-            var dropPin = MKPointAnnotation()
-            dropPin.coordinate = coordinates
-            
-            //apend to array
-            self.annotations.append(dropPin)
-            
-            //add to mapview
-            self.mapView.addAnnotation(dropPin)
-        }
-    }
+    
+//    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+//
+//    }
+//    
+//    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+//        
+//    }
+//    
+//    func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+//        
+//    }
+//    
+//    
+//    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+//
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
